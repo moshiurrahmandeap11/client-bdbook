71a9a450630737be9174e89151c5d26ef05ef33f
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-15 19:55:03 +0600

    feat(app): create auth register page route

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "@/components/auth_components/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <div className="min-h-[80vh] flex items-center justify-center p-4">
+      <RegisterForm />
+    </div>
+  );
+}
+
