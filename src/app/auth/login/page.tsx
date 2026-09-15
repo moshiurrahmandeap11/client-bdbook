ffba1cae21ccb4470fa3c4f40737c3d66a48ca40
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-15 19:55:03 +0600

    feat(app): create auth login page route

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/components/auth_components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-[80vh] flex items-center justify-center p-4">
+      <LoginForm />
+    </div>
+  );
+}
+
