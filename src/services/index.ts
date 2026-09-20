7b6d62cddf39aa583cd07b40b87842b3d6ba28f5
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-20 21:44:05 +0600

    feat(services): create services barrel export in services/index.ts

---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+export * from "./auth.service";
+export * from "./user.service";
+export * from "./post.service";
+export * from "./notification.service";
+export * from "./friend.service";
+export * from "./message.service";
+export * from "./search.service";
+
