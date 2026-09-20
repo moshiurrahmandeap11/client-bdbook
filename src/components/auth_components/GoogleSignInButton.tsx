817b246e2ce76c73617868269043aa21a08af2fa
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-20 14:52:25 +0600

    feat(ui): create GoogleSignInButton component scaffold

---
+++ src/components/auth_components/GoogleSignInButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+interface GoogleSignInButtonProps {
+  text?: string;
+  disabled?: boolean;
+}
+
+// [wip step 1/7]
