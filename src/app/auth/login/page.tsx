99e7f9091782427ae557ce2ff39730e9d0348c2b
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-20 15:43:35 +0600

    refactor(login): adapt login page to render inside new split auth layout

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,6 @@
 import LoginForm from "@/components/auth_components/LoginForm";
 
 export default function LoginPage() {
-  return (
-    <div className="min-h-[80vh] flex items-center justify-center p-4">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 }
-
+// [wip step 1/2]
