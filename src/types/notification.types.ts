fad09e3485aa4835ac119941c6747039db643288
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-20 21:44:04 +0600

    refactor(types): re-export notification types from @/interfaces

--- src/types/notification.types.ts
+++ src/types/notification.types.ts
@@ -1,17 +1,1 @@
-import { IUser } from "./user.types";
-
-export type NotificationType = "LIKE" | "COMMENT" | "FRIEND_REQUEST" | "FRIEND_ACCEPT" | "SYSTEM";
-
-export interface INotification {
-  id: string;
-  _id?: string;
-  userId: string;
-  senderId?: string;
-  sender?: IUser;
-  type: NotificationType;
-  message: string;
-  entityId?: string;
-  isRead: boolean;
-  createdAt: string;
-}
-
+export * from "@/interfaces/notification.interface";
