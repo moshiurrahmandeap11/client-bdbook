f50c214b8216a67e8cf0a0795dcd85991b9827cb
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-20 21:44:04 +0600

    feat(interfaces): create barrel index.ts exporting all interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export * from "./common.interface";
+export * from "./user.interface";
+export * from "./post.interface";
+export * from "./notification.interface";
+export * from "./friend.interface";
+export * from "./message.interface";
+export * from "./auth.interface";
+export * from "./search.interface";
+
