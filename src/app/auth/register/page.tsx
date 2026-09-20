bf340d0b232617eca6b7192cf0b68d74c9fb720e
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-20 15:51:11 +0600

    refactor(register): adapt register page to render inside new split auth layout

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,6 @@
 import RegisterForm from "@/components/auth_components/RegisterForm";
 
 export default function RegisterPage() {
-  return (
-    <div className="min-h-[80vh] flex items-center justify-center p-4">
-      <RegisterForm />
-    </div>
-  );
+  return <RegisterForm />;
 }
-
+// [wip step 1/2]
